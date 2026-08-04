47367ffdb2c7f5a290ce84f563599beba23aee7c
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-08-04 20:58:59 +0300

    extract method

--- Assets/CodeBase/Core/Factory/Grid/GridMap.cs
+++ Assets/CodeBase/Core/Factory/Grid/GridMap.cs
@@ -5,18 +5,28 @@ namespace CodeBase.Core.Factory.Grid
 {
     public class GridMap
     {
-        public ConcurrentDictionary<Vector2Int, FactoryNode> FactoryNodes { get; private set; } = new();
+        public ConcurrentDictionary<Vector2Int, FactoryNode> FactoryNodes { get; } = new();
 
-        public bool TryAddNode(FactoryNode node)
+        public bool CanSetup(Vector2Int position, Vector2Int size)
         {
-            Vector2Int topRight = node.Position + node.Size;
+            Vector2Int topRight = position + size;
             
-            for(int x = node.Position.X; x < topRight.X; x++)
-            for(int y = node.Position.Y; y < topRight.Y; y++)
+            for(int x = position.X; x < topRight.X; x++)
+            for(int y = position.Y; y < topRight.Y; y++)
             {
                 if (FactoryNodes.TryGetValue(new Vector2Int(x, y), out _))
                     return false;
             }
+
+            return true;
+        }
+        
+        public bool TryAddNode(FactoryNode node)
+        {
+            if (!CanSetup(node.Position, node.Size))
+                return false;
+            
+            Vector2Int topRight = node.Position + node.Size;
             
             for(int x = node.Position.X; x < topRight.X; x++)
             for(int y = node.Position.Y; y < topRight.Y; y++)
