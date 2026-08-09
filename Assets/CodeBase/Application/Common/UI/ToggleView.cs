d0a413daf4c16e52e38405cc3dc1043a76e4a694
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-08-09 19:11:26 +0300

    add isExternalInit

--- Assets/CodeBase/Application/Common/UI/ToggleView.cs
+++ Assets/CodeBase/Application/Common/UI/ToggleView.cs
@@ -6,7 +6,7 @@ namespace CodeBase.Application.Common.UI
 {
     public class ToggleView : MonoBehaviour
     {
-        [SerializeField] private Toggle _toggle;
+        [field: SerializeField] public Toggle Toggle { get; private set; }
         [SerializeField] private TMP_Text _label;
 
         public void Setup(string label) => _label.text = label;
